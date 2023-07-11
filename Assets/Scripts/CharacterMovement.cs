e9435dbcbc1b1e1494bc4ab8bc5327f46d6fa700
Juan Segundo Arnaude <juansarnaude@gmail.com>
2023-07-11 18:35:56 -0300

    Death

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.SceneManagement;
 public class CharacterMovement : MonoBehaviour
 {
     public float movementSpeed = 5f;
@@ -7,53 +8,79 @@ public class CharacterMovement : MonoBehaviour
     public Animator animator;
     public ParachuteController parachuteController; // Reference to the ParachuteController script
 
+    public bool isAlive = true;
 
     private Camera characterCamera;
     private float cameraRotationX = 0f;
+
+    [SerializeField] public AudioClip radioSound;
+    [SerializeField] public AudioClip gogogoSound;
+    private AudioSource audioSource;
 
     private void Start()
     {
         characterCamera = GetComponentInChildren<Camera>();
         animator = GetComponent<Animator>();
         parachuteController = GetComponent<ParachuteController>(); // Assign the ParachuteController script
-
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(startingIndicationsSounds());
 
         // Lock cursor to the game window and hide it
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    private void Update()
-
-    
+    private void Update()    
     {
-        // Camera Rotation
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        if (isAlive)
+        {
+            // Camera Rotation
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
 
-        float cameraRotationY = mouseX * rotationSpeed;
-        cameraRotationX -= mouseY * rotationSpeed;
-        cameraRotationX = Mathf.Clamp(cameraRotationX, -90f, 90f);
+            float cameraRotationY = mouseX * rotationSpeed;
+            cameraRotationX -= mouseY * rotationSpeed;
+            cameraRotationX = Mathf.Clamp(cameraRotationX, -90f, 90f);
 
-        characterCamera.transform.localRotation = Quaternion.Euler(cameraRotationX, 0f, 0f);
-        transform.rotation *= Quaternion.Euler(0f, cameraRotationY, 0f);
+            characterCamera.transform.localRotation = Quaternion.Euler(cameraRotationX, 0f, 0f);
+            transform.rotation *= Quaternion.Euler(0f, cameraRotationY, 0f);
 
-        if (parachuteController != null && parachuteController.isFalling){
-            animator.SetBool("Running", false);
-            return;
+            if (parachuteController != null && parachuteController.isFalling){
+                animator.SetBool("Running", false);
+                return;
+            }
+
+            // Character Movement
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = (transform.right * moveHorizontal + transform.forward * moveVertical) * movementSpeed;
+            transform.Translate(movement * Time.deltaTime, Space.World);
+
+            // Set the running parameter in the animator based on character movement
+            bool isRunning = moveHorizontal != 0f || moveVertical != 0f;
+            animator.SetBool("Running", isRunning);
         }
+        else
+        {
+            if(Input.GetKey("r"))
+            {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+            }
+        }
+    }
 
-        // Character Movement
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    private IEnumerator startingIndicationsSounds()
+    {
+        yield return new WaitForSeconds(1f);
+        audioSource.PlayOneShot(radioSound);
+        yield return new WaitForSeconds(2f);
+        audioSource.PlayOneShot(gogogoSound);
+    }
 
-        Vector3 movement = (transform.right * moveHorizontal + transform.forward * moveVertical) * movementSpeed;
-        transform.Translate(movement * Time.deltaTime, Space.World);
-
-        // Set the running parameter in the animator based on character movement
-        bool isRunning = moveHorizontal != 0f || moveVertical != 0f;
-        animator.SetBool("Running", isRunning);
-
-        
+    public void kill()
+    {
+        this.isAlive = false;
     }
 }
--- Assets/Scripts/ParachuteController.cs
+++ Assets/Scripts/ParachuteController.cs
@@ -1,34 +1,52 @@
 using UnityEngine;
-
+using System.Collections;
+using TMPro;
 public class ParachuteController : MonoBehaviour
 {
     public float fallSpeed = 9.8f; // The speed at which the character falls
     public float parachuteFallSpeed = 4f; // The reduced speed when the parachute is activated
-    public float fallThreshold = -1f; // The threshold velocity to determine if the character is falling
+    public float fallThreshold = 1f; // The threshold velocity to determine if the character is falling
     public KeyCode activationKey = KeyCode.Space; // The key to activate the parachute
+    public float startingHeight;
 
     [HideInInspector] public bool isFalling = false; // Made it public so it can be accessed by other scripts
     private Rigidbody rb;
     private GameObject parachute;
     private bool hasLanded = false;
     private Animator animator;
+    private bool wasActivated = false;
+
+    [SerializeField] public AudioClip parachuteOpeningSound;
+    private AudioSource audioSource;
+
+     [SerializeField]public CharacterMovement characterObject;
+
+    [SerializeField] public TMP_Text missionFailedText;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         parachute = transform.Find("Parachute").gameObject;
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        startingHeight = transform.position.y;
     }
 
     private void Update()
     {
-        if (!isFalling && rb.velocity.y < fallThreshold && !hasLanded)
+        if (!isFalling && startingHeight > (transform.position.y + 10f) && !hasLanded)
         {
             isFalling = true;
         }else if (isFalling && !hasLanded)
         {
 
             if(rb.velocity.y == 0){
+                if(wasActivated == false)
+                {//DIED
+                    characterObject.isAlive = false; 
+                    missionFailedText.text = "Mission Failed, soldier \n Press R to restart.";
+                }
                 isFalling = false;
                 parachute.SetActive(false);
                 animator.SetBool("Falling", false);
@@ -37,10 +55,10 @@ public class ParachuteController : MonoBehaviour
 
             }
 
-            if (Input.GetKeyDown(activationKey))
+            if (Input.GetKeyDown(activationKey) && wasActivated == false)
             {
                 ActivateParachute();
-                
+                wasActivated = true; 
             }
 
             // if (isParachuteActivated)
@@ -71,5 +89,16 @@ public class ParachuteController : MonoBehaviour
         parachute.SetActive(true);
         animator.SetBool("Falling", true);
 
+        StartCoroutine(PlaySoundForDuration(parachuteOpeningSound, 2f));
+    }
+
+    private IEnumerator PlaySoundForDuration(AudioClip clip, float duration)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        yield return new WaitForSeconds(duration);
+
+        audioSource.Stop();
     }
 }
